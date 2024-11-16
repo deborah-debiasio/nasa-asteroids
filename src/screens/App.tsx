bd47e35c4ca401e48cc622d4d19d072f823c744a
Deborah De Biasio <>
2024-11-16 16:40:24 +0100

    wips

--- src/screens/App.tsx
+++ src/screens/App.tsx
@@ -1,34 +1,85 @@
 
-
+import moment from 'moment';
 import React, { useEffect, useState } from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, useWindowDimensions, View } from 'react-native';
 import { NasaService } from '../services/NasaService';
+import Carousel from "react-native-reanimated-carousel";
 
 function App(): React.JSX.Element {
- 
-	const [astheroids, setAstheroids] = useState<Array<Astheroid>>();
+	const { width } = useWindowDimensions();
+
+	const scale = 0.7;
+	const PAGE_WIDTH = width * scale;
+	const PAGE_HEIGHT = 240 * scale;
+
+
+	const [astheroids, setAstheroids] = useState<Array<Astheroid>>([]);
 	const getAstheroids = async ()=> {
 		try {
 
-			console.log('HERREE ', process.env.NASA_API_KEY);
-			const response = await NasaService.getAsteroids();
-			console.log('RESP ', response);
+			const todayDate = moment().format("YYYY-MM-DD");
+			const response = await NasaService.getAsteroids(todayDate);
+			if (response?.data?.near_earth_objects[todayDate]) {
+				setAstheroids(response?.data?.near_earth_objects[todayDate]);
+			}
+			console.log('RESP ', response.data.near_earth_objects[todayDate]);
 		} catch (e: any) {
 			console.log('ERROR ', e);
 		}
 	}
+
+	// const AutoPLay = useToggleButton({
+	// 	defaultValue: false,
+	// 	buttonTitle: ElementsText.AUTOPLAY,
+	// });
+ 
+	// const animationStyle = React.useCallback((value: number) => {
+	// 	"worklet";
+ 
+	// 	const zIndex = interpolate(value, [-1, 0, 1], [10, 20, 30]);
+	// 	const rotateZ = `${interpolate(value, [-1, 0, 1], [-45, 0, 45])}deg`;
+	// 	const translateX = interpolate(
+	// 		value,
+	// 		[-1, 0, 1],
+	// 		[-useWindowDimensions().width, 0, useWindowDimensions().width],
+	// 	);
+ 
+	// 	return {
+	// 		transform: [{ rotateZ }, { translateX }],
+	// 		zIndex,
+	// 	};
+	// }, []);
 
 	useEffect(() => {
 
 		getAstheroids();
 	}, []);
 
-	return (
-		<SafeAreaView>
-		
-		</SafeAreaView>
-	);
+	return <SafeAreaView>
+		<View
+			id="carousel-component"
+			// dataSet={{ kind: "custom-animations", name: "rotate-in-out" }}
+		>
+			<Carousel
+				loop
+				style={{
+					width: width,
+					height: 240,
+					justifyContent: "center",
+					alignItems: "center",
+				}}
+				width={PAGE_WIDTH}
+				height={PAGE_HEIGHT}
+				data={astheroids ?? []}
+				renderItem={({item, index }) => <Text>{item.name}</Text>}
+				// autoPlay={AutoPLay.status}
+				// customAnimation={animationStyle}
+			/>
+		</View>
+	</SafeAreaView>;
 }
+
+export default App;
 
 const styles = StyleSheet.create({
 	sectionContainer: {
@@ -48,5 +99,3 @@ const styles = StyleSheet.create({
 		fontWeight: '700',
 	},
 });
-
-export default App;
--- src/services/NasaService.ts
+++ src/services/NasaService.ts
@@ -5,9 +5,8 @@ import { ApiBaseService } from "./ApiBaseService";
 export class NasaService implements ApiBaseService {
 
     // Retrieve a list of Asteroids based on their closest approach date to Earth
-    static async getAsteroids() {
-        const todayDate = moment().format("YYYY-MM-DD");
-        return await ApiBaseService.request('GET', `${BaseUrl}feed?start_date=${todayDate}&end_date=${todayDate}&api_key=${ApiKey}`);
+    static async getAsteroids(date: String) {
+        return await ApiBaseService.request('GET', `${BaseUrl}feed?start_date=${date}&end_date=${date}&api_key=${ApiKey}`);
     }
 
 }
